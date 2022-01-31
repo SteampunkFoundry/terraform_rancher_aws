8fc6776a85349f3b00a89f59773ba278f9aba76d
steampunk-vic <victor.konn@steampunk.com>
2022-01-31 01:07:03 -0500

    add aws profile for sso; split rancher2 provider into bootstrap and admin

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 terraform {
   backend "s3" {
     bucket = "webmod-tfstate"
-    key    = "infra/rancher"
+    key    = "infra/test-rancher"
     region = "us-east-1"
+    profile = "Webmod-Admin"
   }
 }
 
 provider "aws" {
   region = var.aws_region
+  profile = var.profile
 }
 
 provider "helm" {
@@ -17,7 +19,15 @@ provider "helm" {
 }
 
 provider "rancher2" {
+  alias = "bootstrap"
   api_url = "https://${var.rancher_server_dns}"
   bootstrap = true
+  insecure = true
+}
+
+provider "rancher2" {
+  alias = "admin"
+  api_url = "https://${var.rancher_server_dns}"
+  token_key = rancher2_bootstrap.admin.token
   insecure = true
 }
