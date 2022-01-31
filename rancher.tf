705073a1ae2e389aa5ca25b7c0ed17d276c41f1c
steampunk-vic <victor.konn@steampunk.com>
2022-01-31 01:11:12 -0500

    update tfvars to reflect var changes; add random pass resource; add aws secret manager resource; add rancher bootstrap, cloud credentials, and node template resources

--- rancher.tf
+++ rancher.tf
@@ -1,3 +1,28 @@
+resource "random_password" "rancher_admin" {
+  length  = 12
+  lower   = true
+  number  = true
+  special = true
+  override_special = "!@#$%^&*"
+}
+
+resource "aws_secretsmanager_secret" "rancher" {
+  name = "RancherAdminTest2"
+  description = "Rancher server admin user and password for ${var.rancher_server_dns}"
+  recovery_window_in_days = 0
+  tags = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "secret_contents" {
+  secret_id = aws_secretsmanager_secret.rancher.id
+  secret_string = <<EOF
+   {
+    "username": "admin",
+    "password": "${random_password.rancher_admin.result}"
+   }
+  EOF
+}
+
 resource "helm_release" "cert_manager" {
   repository       = "https://charts.jetstack.io"
   name             = "cert-manager"
@@ -38,23 +63,56 @@ resource "helm_release" "rancher_server" {
     value = "admin"
   }
   depends_on = [
-    helm_release.cert_manager
+    helm_release.cert_manager,
+    random_password.rancher_admin
   ]
 }
 
-# # Rancher certificates
-# data "kubernetes_secret" "rancher_cert" {
-#   depends_on = [helm_release.rancher_server]
-
-#   metadata {
-#     name      = "tls-rancher-ingress"
-#     namespace = "cattle-system"
-#   }
-# }
-
 resource "rancher2_bootstrap" "admin" {
-  password  = var.rancher_password
+  provider = rancher2.bootstrap
+  initial_password = "admin"
+  password  = random_password.rancher_admin.result
   depends_on = [
-    helm_release.rancher_server
+    helm_release.rancher_server,
+    random_password.rancher_admin
   ]
+}
+
+resource "rancher2_cloud_credential" "cloud_credential" {
+  provider = rancher2.admin
+  name = "rancher_cluster_credentials"
+  description = "Credentials for user ${module.rancher_user.iam_user_name}"
+  amazonec2_credential_config {
+    access_key = module.rancher_user.iam_access_key_id
+    secret_key = module.rancher_user.iam_access_key_secret
+    default_region = var.aws_region
+  }
+  depends_on = [
+    rancher2_bootstrap.admin,
+    module.rancher_user
+  ]
+}
+
+resource "rancher2_node_template" "node_template" {
+  provider = rancher2.admin
+  name = "wedbmod template"
+  description = "Template used to provision WebMod cluster"
+  engine_install_url = "https://releases.rancher.com/install-docker/${var.docker_version}.sh"
+  amazonec2_config {
+    access_key = rancher2_cloud_credential.cloud_credential.amazonec2_credential_config[0].access_key
+    secret_key = rancher2_cloud_credential.cloud_credential.amazonec2_credential_config[0].secret_key
+    ami =  data.aws_ami.ubuntu_20_04.id
+    region = var.aws_region
+    security_group = ["rancher nodes"]
+    subnet_id = var.subnet_id
+    vpc_id = var.vpc_id
+    zone = trimprefix(data.aws_instance.rancher_instance.availability_zone, var.aws_region)
+    encrypt_ebs_volume = true
+    iam_instance_profile = module.iam_role_child_clusters.iam_role_name
+    instance_type = var.instance_type
+    kms_key = data.aws_kms_alias.ebs.target_key_id
+    private_address_only = true
+    volume_type = "gp3"
+    tags = "Rancher Provisioned,true"
+  }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,32 +1,26 @@
 # General AWS variables
 aws_region    = "us-east-1"
+profile       = "Webmod-Admin"
+name          = "TEST-webmod-rancher"
 instance_type = "t3a.medium"
 key_name      = "webmod-rancher"
-name          = "webmod-rancher"
 tags = {
   Terraform   = "true"
+  Team        = "Webmod"
+  Environment = "dev"
 }
 
 # VPC variables
 vpc_id          = "vpc-03dc11277661baafb"
 subnet_id       = "subnet-09dee7714b308cd9a" // Private subnet 1
-private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
-public_subnets  = ["10.0.100.0/19"]
-
-# RDS variables
-#db_instance_type = "db.t3.medium"
-#db_storage       = "20"
-#db_password      = ""
-#db_name          = "datastore"
 
 # Rancher variables
-private_ip           = "10.0.0.25"
+private_ip           = "10.0.0.100"
 docker_version       = "20.10"
 k3s_version          = "v1.21.5+k3s1"
 cert_manager_version = "v1.6.1"
-rancher_version      = "v2.6.3"
-rancher_server_dns   = "rancher.webmod.private"
-#rancher_password     = ""
+rancher_version      = "v2.5.3"
+rancher_server_dns   = "test.webmod.private"
 
 # ssh variables
 instance_username = "ubuntu"
